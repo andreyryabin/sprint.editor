dc4dea3859ec7b69762ad6efe580d1d2a294b0ab
Andrey Ryabin <andreyryabin@yandex.ru>
2017-04-03 20:35:42 +0500

    Версия 1.0.14

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.0.12",
-    "VERSION_DATE" => "2017-03-15 18:00:00",
+    "VERSION" => "1.0.14",
+    "VERSION_DATE" => "2017-04-02 12:00:00",
 );
