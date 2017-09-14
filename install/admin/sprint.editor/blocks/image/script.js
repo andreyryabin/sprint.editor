84dd0777c27103dac55737ab62c995934e16238b
Andrey Ryabin <andreyryabin@yandex.ru>
2017-09-14 17:09:58 +0500

    release-1.1.9

--- install/admin/sprint.editor/blocks/image/script.js
+++ install/admin/sprint.editor/blocks/image/script.js
@@ -18,12 +18,12 @@ sprint_editor.registerBlock('image', function($, $el, data) {
 
         renderfiles();
 
-        var btn = $el.find('.sp-file');
-        var btninput = btn.find('input[type=file]');
-        var label = btn.find('span');
-        var labeltext = label.text();
+        var $btn = $el.find('.sp-file');
+        var $btninput = $btn.find('input[type=file]');
+        var $label = $btn.find('strong');
+        var labeltext = $label.text();
 
-        btninput.fileupload({
+        $btninput.fileupload({
             url: sprint_editor.getBlockWebPath('image') + '/upload.php',
             dataType: 'json',
             done: function (e, result) {
@@ -36,10 +36,10 @@ sprint_editor.registerBlock('image', function($, $el, data) {
             progressall: function (e, result) {
                 var progress = parseInt(result.loaded / result.total * 100, 10);
 
-                label.text('Загрузка: ' + progress + '%');
+                $label.text('Загрузка: ' + progress + '%');
 
                 if (progress>=100){
-                    label.text(labeltext);
+                    $label.text(labeltext);
                 }
             }
         }).prop('disabled', !$.support.fileInput)
