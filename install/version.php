c58fb367e7187f4a8ba6341a71a22e5351649abf
Andrey Ryabin <andreyryabin@yandex.ru>
2017-09-14 17:01:20 +0500

    release-1.1.9

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.1.8",
-    "VERSION_DATE" => "2017-09-10 12:00:00",
+    "VERSION" => "1.1.9",
+    "VERSION_DATE" => "2017-09-14 12:00:00",
 );
