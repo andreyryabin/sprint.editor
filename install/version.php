45be8fbb964db5b446db3226293393125603bfd2
Andrey Ryabin <andreyryabin@yandex.ru>
2018-12-04 20:02:58 +0500

    2.3.5

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "2.3.4",
-    "VERSION_DATE" => "2018-12-03 18:00:00",
+    "VERSION" => "2.3.5",
+    "VERSION_DATE" => "2018-12-03 19:00:00",
 );
