e7e4dfd65f45bb3707e77392345eacefe73f934b
Andrey Ryabin <andreyryabin@yandex.ru>
2019-05-29 20:26:02 +0500

    2.3.11

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "2.3.9",
-    "VERSION_DATE" => "2019-02-14 10:00:00",
+    "VERSION" => "2.3.11",
+    "VERSION_DATE" => "2019-05-29 10:00:00",
 );
--- updater.php
+++ updater.php
@@ -27,6 +27,6 @@ if ($updater && $updater instanceof \CUpdater) {
         $updater->CopyFiles("install/admin/", "admin/");
     }
 
-    //2.3.9
+    //2.3.11
 
 }
