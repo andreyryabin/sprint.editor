8d08684ab9108b96d34fd9d4d55001dfd717a918
Andrey Ryabin <andreyryabin@yandex.ru>
2019-07-22 14:51:46 +0500

    2.4.3

--- install/components/sprint.editor/blocks/templates/.default/_grid.php
+++ install/components/sprint.editor/blocks/templates/.default/_grid.php
@@ -6,7 +6,7 @@
 <?if (count($layout['columns']) == 1 && empty($layout['columns'][0]['css'])): ?>
     <? $this->includeLayoutBlocks( 0) ?>
 <? else: ?>
-<div class="container">
+<div class="sp-container">
     <div class="row">
         <? foreach ($layout['columns'] as $columnIndex => $column):?>
             <div<?if (!empty($column['css'])):?> class="<?= $column['css'] ?>"<?endif?>><? $this->includeLayoutBlocks($columnIndex) ?></div>
