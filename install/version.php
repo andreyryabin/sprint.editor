58aca97e512054c39685219657e25502a8897de8
Andrey Ryabin <andreyryabin@yandex.ru>
2021-10-06 19:26:08 +0500

    3.19.2

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    "VERSION"      => "3.19.1",
-    "VERSION_DATE" => "2021-10-06 10:30:00",
+    "VERSION"      => "3.19.2",
+    "VERSION_DATE" => "2021-10-06 10:40:00",
 ];
