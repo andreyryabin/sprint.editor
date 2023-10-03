0e0c6dcd5e2e7266447684f16c8ceea64946c1b5
Andrey Ryabin <andreyryabin@yandex.ru>
2023-10-03 15:17:23 +0500

    4.4.2

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    "VERSION"      => "4.4.1",
-    "VERSION_DATE" => "2023-09-30 10:30:00",
+    "VERSION"      => "4.4.2",
+    "VERSION_DATE" => "2023-09-30 10:40:00",
 ];
--- updater.php
+++ updater.php
@@ -24,6 +24,6 @@ if ($updater && $updater instanceof \CUpdater) {
     if (is_dir(__DIR__ . '/install/admin/')) {
         $updater->CopyFiles("install/admin/", "admin/");
     }
-    //4.3.1
+    //4.4.2
 
 }
