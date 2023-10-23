774fa31767b42557a8dcc12043c92aa446e9e8cf
Andrey Ryabin <andreyryabin@yandex.ru>
2023-10-23 14:19:08 +0500

    4.5.6

--- install/components/sprint.editor/blocks/templates/.default/complex_video_text.php
+++ install/components/sprint.editor/blocks/templates/.default/complex_video_text.php
@@ -1,8 +1,6 @@
 <?php /** @var $block array */ ?>
 <div class="sp-video-text">
     <?php
-
-    echo "<pre>";print_r($block);/*debug*/echo "</pre>";
     $this->includeBlock(array_merge($block['video'], ['name' => 'video']));
     $this->includeBlock(array_merge($block['text'], ['name' => 'text']));
     ?>
