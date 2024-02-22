62ac8a2ce5b5314e40b2238df9f8b3d549ba45a2
Andrey Ryabin <andreyryabin@yandex.ru>
2024-02-22 22:50:05 +0500

    4.11.1

--- admin/pages/support.php
+++ admin/pages/support.php
@@ -7,10 +7,11 @@ global $APPLICATION;
 $APPLICATION->SetTitle(GetMessage('SPRINT_EDITOR_SUPPORT'));
 Asset::getInstance()->addJs('/bitrix/admin/sprint.editor/assets/support_page.js');
 
-$content = file_get_contents(
-    'https://andreyryabin.github.io/sprint_editor/support.html',
-    false,
-    stream_context_create(['http' => ['timeout' => 10]])
-);
-
-echo $content;
+if (function_exists('curl_init')) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://andreyryabin.github.io/sprint_editor/support.html');
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    echo curl_exec($ch);
+    curl_close($ch);
+}
