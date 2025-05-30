1629898095b9a509dc6faca0e2f0712b53d1345b
Andrey Ryabin <andreyryabin@yandex.ru>
2025-05-30 14:02:03 +0500

    4.24.0

--- install/components/sprint.editor/blocks/templates/.default/complex_select_text.php
+++
@@ -1,8 +0,0 @@
-<?php 
-/**
- * @var $block array
- * @var $this SprintEditorBlocksComponent
- */
-?><div class="sp-complex sp-complex_select_text">
-<?php $this->includeBlock($block['textfield']);?>
-</div>
