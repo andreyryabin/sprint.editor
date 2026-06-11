cfaa88fa3e0197f2a6a320bdc34020f4adb3c83e
Andrey Ryabin <andreyryabin@yandex.ru>
2026-06-11 15:35:06 +0500

    5.0.2

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    "VERSION"      => "5.0.0",
-    "VERSION_DATE" => "2026-06-08 10:50:40",
+    "VERSION"      => "5.0.2",
+    "VERSION_DATE" => "2026-06-10 11:00:00",
 ];
